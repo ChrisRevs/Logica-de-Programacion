4fe28e1c1d62a6c914c76bce784967a66bc90877
ChrisRevs <150978807+ChrisRevs@users.noreply.github.com>
2023-12-23 16:53:09 -0600

    Update Tijeras.py

--- Tijeras.py
+++ Tijeras.py
@@ -4,17 +4,17 @@ from time import sleep
 op = ["piedra", "papel", "tijeras"]
 sep = "-" * 15
 
-while True:
+while True: #Utilizo un codigo while true para repetir el juego de manera indefinida 
     user = input("elije: piedra. papel o tijeras: ").lower()
     sleep(0.9)
-    if user not in op:
+    if user not in op: #uso de if y not in de caracter tipo boolean
         print("\nMovimiento no valido")
         continue
     pc = random.choice(op)
-    sleep(0.9)
+    sleep(0.9) #sleep para provocar un tiempo de espera antes de mostrar el resultado 
     print(f"\nLa Pc Selecciono {pc}")
     sleep(0.9)
-    if user == pc:
+    if user == pc: #variante boolean tipo true 
         print(f"\nEmpate, ambos seleccionaron {user}")
     if user == "piedra" and pc == "tijeras":
         print(f"\nganaste, {user} ganas en contra de {pc}")
@@ -22,7 +22,7 @@ while True:
         print(f"\nganaste, {user} ganas en contra de {pc}")
     if user == "papel" and pc == "piedra":
         print(f"\nganaste, {user} ganas en contra de {pc}")
-    else:
+    else: #variante boolean false 
         print(f"\nPerdiste, {user} pierdes contra {pc}")
         sleep(0.9)
         print(f"{sep} fin del Juego {sep}")
